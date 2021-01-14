85fb0798d35449e6e0b9d2b24f4bf0ce923f096a
LUS1N <sinkus.lukas@gmail.com>
2021-01-14 08:04:35 +0100

    implement parsing input

--- src/Pyramid.Console/Application/ConsoleApplication.cs
+++ src/Pyramid.Console/Application/ConsoleApplication.cs
@@ -1,11 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Logging;
+
 namespace Pyramid.Console.Application
 {
     public class ConsoleApplication : IApplication
     {
-        
+        private readonly ILogger<ConsoleApplication> _logger;
+
+        public ConsoleApplication(ILogger<ConsoleApplication> logger)
+        {
+            _logger = logger;
+        }
+
         public void Run(string[] args)
         {
-            throw new System.NotImplementedException();
+            _logger.LogInformation("Running with args {@args}", args);
+            var filePath = GetFilePath(args);
+            // parse input from file in path
+            // pass input integers to pyramid
+            // output results to consoles
+            args.ToList().ForEach(System.Console.WriteLine);
         }
+
+        private static string GetFilePath(IReadOnlyList<string> args) =>
+            args.FirstOrDefault() ??
+            throw new ArgumentException("File path needs to be passed as input");
     }
 }
